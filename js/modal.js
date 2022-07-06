5adefa829e770944b96413e911663096bc53c39c
Iryna Zaitseva <zaitseva02@gmail.com>
2022-07-07 00:13:36 +0300

    commit первый

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,7 @@
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
+      console.log("TOGGLe");
       refs.modal.classList.toggle("is-hidden");
     }
   })();
